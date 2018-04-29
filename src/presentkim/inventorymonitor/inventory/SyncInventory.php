e2d819e538ddd4d749da9a66b55bb0d339afdd89
ZPlayzMCPE <jameshayward3800@gmail.com>
2018-04-29 12:28:30 +0100

    Major Updates
    Added Armour support.
    Fixed undefined issues.
    Bug fixes and improvements.

--- src/presentkim/inventorymonitor/inventory/SyncInventory.php
+++ src/presentkim/inventorymonitor/inventory/SyncInventory.php
@@ -25,33 +25,44 @@ use presentkim\inventorymonitor\task\SendDataPacketTask;
 use presentkim\inventorymonitor\util\Translation;
 
 class SyncInventory extends CustomInventory{
-  
+
     /** @var NetworkLittleEndianNBTStream|null */
     private static $nbtWriter = null;
+
     /** @var self[] */
     public static $instances = [];
+
     /** CompoundTag */
     private $nbt;
+
     /** Vector3[] */
     private $vectors = [];
+
     /** @var string */
     private $playerName;
+
     /**
      * SyncInventory constructor.
      *
      * @param string      $playerName
      * @param CompoundTag $namedTag
      */
-  
     public function __construct(string $playerName, ?CompoundTag $namedTag = null){
         $items = [];
         $player = Server::getInstance()->getPlayerExact($playerName);
         if ($player instanceof Player) {
             $inventory = $player->getInventory();
-            for ($i = 0, $size = $inventory->getSize(); $i < $size; ++$i) {
+            for ($i = 0; $i < 36; ++$i) {
                 $item = $inventory->getItem($i);
                 if (!$item->isNull()) {
                     $items[$i] = $item;
+                }
+            }
+            $armorInventory = $player->getArmorInventory();
+            for ($i = 0; $i < 4; ++$i) {
+                $item = $armorInventory->getItem($i);
+                if (!$item->isNull()) {
+                    $items[$i + 46] = $item;
                 }
             }
         } elseif ($namedTag !== null) {
@@ -60,13 +71,24 @@ class SyncInventory extends CustomInventory{
                 /** @var CompoundTag $itemTag */
                 foreach ($inventoryTag as $i => $itemTag) {
                     $slot = $itemTag->getByte("Slot");
-                    if ($slot >= 9 && $slot < 100) {
+                    if ($slot > 8 && $slot < 44) { // 9-44 is PlayerInventory slot
                         $items[$slot - 9] = Item::nbtDeserialize($itemTag);
+                    } elseif ($slot > 99 and $slot < 104) { // 100-103 is ArmorInventory slot
+                        // $items[$slot - 100 + 44] = Item::nbtDeserialize($itemTag);
+                        $items[$slot - 54] = Item::nbtDeserialize($itemTag);
                     }
                 }
             }
         }
+        $borderItem = Item::get(Block::SKULL_BLOCK);
+        $borderItem->setCustomName('');
+        for ($i = 36; $i < 54; ++$i) {
+            if (!$this->isValidSlot($i)) {
+                $items[$i] = clone $borderItem;
+            }
+        }
         parent::__construct(new Vector3(0, 0, 0), $items, 54, null);
+
         $this->playerName = $playerName;
         $this->nbt = new CompoundTag('', [
           new StringTag('id', 'Chest'),
@@ -75,18 +97,22 @@ class SyncInventory extends CustomInventory{
           new IntTag('z', 0),
           new StringTag('CustomName', Translation::translate('chest-name', $player instanceof Player ? $player->getName() : $playerName)),
         ]);
+
         if (self::$nbtWriter === null) {
             self::$nbtWriter = new NetworkLittleEndianNBTStream();
         }
     }
+
     /** @param Player $who */
     public function onOpen(Player $who) : void{
         BaseInventory::onOpen($who);
+
         $this->vectors[$key = $who->getLowerCaseName()] = $who->subtract(0, 3, 0)->floor();
         if ($this->vectors[$key]->y < 0) {
             $this->vectors[$key]->y = 0;
         }
         $vec = $this->vectors[$key];
+
         for ($i = 0; $i < 2; $i++) {
             $pk = new UpdateBlockPacket();
             $pk->x = $vec->x + $i;
@@ -95,11 +121,14 @@ class SyncInventory extends CustomInventory{
             $pk->blockRuntimeId = BlockFactory::toStaticRuntimeId(Block::CHEST);
             $pk->flags = UpdateBlockPacket::FLAG_NONE;
             $who->sendDataPacket($pk);
+
+
             $this->nbt->setInt('x', $vec->x + $i);
             $this->nbt->setInt('y', $vec->y);
             $this->nbt->setInt('z', $vec->z);
             $this->nbt->setInt('pairx', $vec->x + (1 - $i));
             $this->nbt->setInt('pairz', $vec->z);
+
             $pk = new BlockEntityDataPacket();
             $pk->x = $vec->x + $i;
             $pk->y = $vec->y;
@@ -107,6 +136,7 @@ class SyncInventory extends CustomInventory{
             $pk->namedtag = self::$nbtWriter->write($this->nbt);
             $who->sendDataPacket($pk);
         }
+
         $pk = new ContainerOpenPacket();
         $pk->type = WindowTypes::CONTAINER;
         $pk->entityUniqueId = -1;
@@ -114,17 +144,23 @@ class SyncInventory extends CustomInventory{
         $pk->y = $vec->y;
         $pk->z = $vec->z;
         $pk->windowId = $who->getWindowId($this);
+
         $pk2 = new InventoryContentPacket();
         $pk2->items = $this->getContents(true);
         $pk2->windowId = $pk->windowId;
         Server::getInstance()->getScheduler()->scheduleDelayedTask(new SendDataPacketTask($who, $pk, $pk2), 5);
     }
+
     /** @param Player $who */
     public function onClose(Player $who) : void{
         BaseInventory::onClose($who);
         $key = $who->getLowerCaseName();
+        if (!isset($this->vectors[$key])) {
+            return;
+        }
         for ($i = 0; $i < 2; $i++) {
             $block = $who->getLevel()->getBlock($vec = $this->vectors[$key]->add($i, 0, 0));
+
             $pk = new UpdateBlockPacket();
             $pk->x = $vec->x;
             $pk->y = $vec->y;
@@ -132,6 +168,7 @@ class SyncInventory extends CustomInventory{
             $pk->blockRuntimeId = BlockFactory::toStaticRuntimeId($block->getId(), $block->getDamage());
             $pk->flags = UpdateBlockPacket::FLAG_NONE;
             $who->sendDataPacket($pk);
+
             $tile = $who->getLevel()->getTile($vec);
             if ($tile instanceof Spawnable) {
                 $who->sendDataPacket($tile->createSpawnPacket());
@@ -139,6 +176,7 @@ class SyncInventory extends CustomInventory{
         }
         unset($this->vectors[$key]);
     }
+
     /**
      * @param int  $index
      * @param Item $item
@@ -152,27 +190,74 @@ class SyncInventory extends CustomInventory{
             $player = Server::getInstance()->getPlayerExact($this->playerName);
             if ($player !== null) {
                 $inventory = $player->getInventory();
-                if ($index < 36) { // 36 = PlayerInventory::getDefaultSize();
+                if ($this->isPlayerSlot($index)) {
                     $inventory->setItem($index, $item, true);
+                } elseif ($this->isArmorSlot($index)) {
+                    $player->getArmorInventory()->setItem($index - 46, $item, true);
+                } elseif ($this->isCursorSlot($index)) {
+                    $player->getCursorInventory()->setItem(0, $item, true);
                 }
             }
         }
         return parent::setItem($index, $item, $send);
     }
+
     /** @return string */
     public function getName() : string{
         return "SyncInventory";
     }
+
     /** @return int */
     public function getDefaultSize() : int{
         return 54;
     }
+
     /** @return int */
     public function getNetworkType() : int{
         return WindowTypes::CONTAINER;
     }
+
     /** @return string */
     public function getPlayerName() : string{
         return $this->playerName;
+    }
+
+    /**
+     * @param int $index
+     *
+     * @return bool
+     */
+    public function isValidSlot(int $index){
+        return $this->isPlayerSlot($index) || $this->isArmorSlot($index) || $this->isCursorSlot($index);
+    }
+
+    /**
+     * @param int $index
+     *
+     * @return bool
+     */
+    public function isPlayerSlot(int $index){
+        //  0-36 is PlayerInventory slot
+        return $index < 36;
+    }
+
+    /**
+     * @param int $index
+     *
+     * @return bool
+     */
+    public function isArmorSlot(int $index){
+        // 46-49 is ArmorInventory  slot
+        return $index > 45 && $index < 50;
+    }
+
+    /**
+     * @param int $index
+     *
+     * @return bool
+     */
+    public function isCursorSlot(int $index){
+        // 52 is ArmorInventory  slot
+        return $index === 52;
     }
 }
--- src/presentkim/inventorymonitor/listener/InventoryEventListener.php
+++ src/presentkim/inventorymonitor/listener/InventoryEventListener.php
@@ -4,19 +4,24 @@ namespace presentkim\inventorymonitor\listener;
 
 use pocketmine\Player;
 use pocketmine\event\Listener;
-use pocketmine\event\entity\EntityInventoryChangeEvent;
+use pocketmine\event\entity\{
+  EntityArmorChangeEvent, EntityInventoryChangeEvent
+};
 use pocketmine\event\inventory\InventoryTransactionEvent;
 use pocketmine\inventory\transaction\action\SlotChangeAction;
+use pocketmine\inventory\PlayerCursorInventory;
 use presentkim\inventorymonitor\InventoryMonitor as Plugin;
 use presentkim\inventorymonitor\inventory\SyncInventory;
 
 class InventoryEventListener implements Listener{
-    
+
     /** @var Plugin */
     private $owner = null;
+
     public function __construct(){
         $this->owner = Plugin::getInstance();
     }
+
     /**
      * @priority MONITOR
      *
@@ -28,11 +33,12 @@ class InventoryEventListener implements Listener{
             if ($player instanceof Player) {
                 $syncInventory = SyncInventory::$instances[$player->getLowerCaseName()] ?? null;
                 if ($syncInventory !== null) {
-                    $syncInventory->setItem($event->getSlot(), $event->getNewItem(), true, false);
+                    $syncInventory->setItem($event->getSlot() + ($event instanceof EntityArmorChangeEvent ? 46 : 0), $event->getNewItem(), true, false);
                 }
             }
         }
     }
+
     /**
      * @priority MONITOR
      *
@@ -41,9 +47,18 @@ class InventoryEventListener implements Listener{
     public function onInventoryTransactionEvent(InventoryTransactionEvent $event){
         $transaction = $event->getTransaction();
         foreach ($transaction->getActions() as $key => $action) {
-            if ($action instanceof SlotChangeAction && $action->getInventory() instanceof SyncInventory) {
-                if ($action->getSlot() >= 36) {// 36 = PlayerInventory::getDefaultSize();
-                    $event->setCancelled();
+            if ($action instanceof SlotChangeAction) {
+                $inventory = $action->getInventory();
+                if ($inventory instanceof SyncInventory) {
+                    if (!$inventory->isValidSlot($action->getSlot())) {
+                        $event->setCancelled();
+                    }
+                } elseif ($inventory instanceof PlayerCursorInventory) {
+                    $player = $inventory->getHolder();
+                    $syncInventory = SyncInventory::$instances[$player->getLowerCaseName()] ?? null;
+                    if ($syncInventory !== null) {
+                        $syncInventory->setItem(52, $action->getTargetItem(), true, false);
+                    }
                 }
             }
         }
