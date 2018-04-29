65e97579b05e1dc75eb1ad1ee174962793c55da5
ZPlayzMCPE <jameshayward3800@gmail.com>
2018-04-29 09:13:18 +0100

    Updates

--- src/presentkim/inventorymonitor/listener/InventoryEventListener.php
+++ src/presentkim/inventorymonitor/listener/InventoryEventListener.php
@@ -5,18 +5,18 @@ namespace presentkim\inventorymonitor\listener;
 use pocketmine\Player;
 use pocketmine\event\Listener;
 use pocketmine\event\entity\EntityInventoryChangeEvent;
+use pocketmine\event\inventory\InventoryTransactionEvent;
+use pocketmine\inventory\transaction\action\SlotChangeAction;
 use presentkim\inventorymonitor\InventoryMonitor as Plugin;
 use presentkim\inventorymonitor\inventory\SyncInventory;
 
 class InventoryEventListener implements Listener{
-
+    
     /** @var Plugin */
     private $owner = null;
-
     public function __construct(){
         $this->owner = Plugin::getInstance();
     }
-
     /**
      * @priority MONITOR
      *
@@ -29,6 +29,21 @@ class InventoryEventListener implements Listener{
                 $syncInventory = SyncInventory::$instances[$player->getLowerCaseName()] ?? null;
                 if ($syncInventory !== null) {
                     $syncInventory->setItem($event->getSlot(), $event->getNewItem(), true, false);
+                }
+            }
+        }
+    }
+    /**
+     * @priority MONITOR
+     *
+     * @param InventoryTransactionEvent $event
+     */
+    public function onInventoryTransactionEvent(InventoryTransactionEvent $event){
+        $transaction = $event->getTransaction();
+        foreach ($transaction->getActions() as $key => $action) {
+            if ($action instanceof SlotChangeAction && $action->getInventory() instanceof SyncInventory) {
+                if ($action->getSlot() >= 36) {// 36 = PlayerInventory::getDefaultSize();
+                    $event->setCancelled();
                 }
             }
         }
