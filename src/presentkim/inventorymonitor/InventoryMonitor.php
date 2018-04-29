6c131e4bba99ade161c584ebc4eb8209bc35559d
ZPlayzMCPE <jameshayward3800@gmail.com>
2018-04-29 09:15:13 +0100

    Updates

--- src/presentkim/inventorymonitor/InventoryMonitor.php
+++ src/presentkim/inventorymonitor/InventoryMonitor.php
@@ -16,49 +16,42 @@ use presentkim\inventorymonitor\listener\{
 use presentkim\inventorymonitor\util\Translation;
 
 class InventoryMonitor extends PluginBase{
-
-    /** @var self */
+    /** @var InventoryMonitor */
     private static $instance = null;
-
     /** @var string */
     public static $prefix = '';
-
-    /** @return self */
-    public static function getInstance() : self{
+    /** @return InventoryMonitor */
+    public static function getInstance() : InventoryMonitor{
         return self::$instance;
     }
-
     /** @var PoolCommand */
     private $command;
-
     public function onLoad() : void{
         if (self::$instance === null) {
             self::$instance = $this;
             Translation::loadFromResource($this->getResource('lang/eng.yml'), true);
         }
     }
-
     public function onEnable() : void{
         $this->load();
         $this->getServer()->getPluginManager()->registerEvents(new InventoryEventListener(), $this);
         $this->getServer()->getPluginManager()->registerEvents(new PlayerEventListener(), $this);
     }
-
     public function onDisable() : void{
         foreach (SyncInventory::$instances as $playerName => $syncInventory) {
             $player = $this->getServer()->getPlayerExact($playerName);
             if ($player !== null) {
                 $inventory = $player->getInventory();
-                for ($i = 0; $i < 36; ++$i) {
+                for ($i = 0; $i < 36; ++$i) { // 36 = PlayerInventory::getDefaultSize();
                     $inventory->setItem($i, $syncInventory->getItem($i));
                 }
             } else {
                 $namedTag = $this->getServer()->getOfflinePlayerData($playerName);
                 $inventoryTag = new ListTag("Inventory", [], NBT::TAG_Compound);
-                for ($i = 0; $i < 36; ++$i) {
+                for ($i = 0; $i < 36; ++$i) { // 36 = PlayerInventory::getDefaultSize();
                     $item = $syncInventory->getItem($i);
                     if (!$item->isNull()) {
-                        $inventoryTag[$i + 9] = $item->nbtSerialize($i + 9);
+                        $inventoryTag->push($item->nbtSerialize($i + 9));
                     }
                 }
                 $namedTag->setTag($inventoryTag);
@@ -70,13 +63,11 @@ class InventoryMonitor extends PluginBase{
         }
         SyncInventory::$instances = [];
     }
-
     public function load() : void{
         $dataFolder = $this->getDataFolder();
         if (!file_exists($dataFolder)) {
             mkdir($dataFolder, 0777, true);
         }
-
         $langfilename = $dataFolder . 'lang.yml';
         if (!file_exists($langfilename)) {
             $resource = $this->getResource('lang/eng.yml');
@@ -86,11 +77,9 @@ class InventoryMonitor extends PluginBase{
         } else {
             Translation::load($langfilename);
         }
-
         self::$prefix = Translation::translate('prefix');
         $this->reloadCommand();
     }
-
     public function reloadCommand() : void{
         if ($this->command == null) {
             $this->command = new PoolCommand($this, 'inventorymonitor');
@@ -105,7 +94,6 @@ class InventoryMonitor extends PluginBase{
         }
         $this->getServer()->getCommandMap()->register(strtolower($this->getName()), $this->command);
     }
-
     /**
      * @param string $name = ''
      *
@@ -113,10 +101,5 @@ class InventoryMonitor extends PluginBase{
      */
     public function getCommand(string $name = '') : PoolCommand{
         return $this->command;
-    }
-
-    /** @param PoolCommand $command */
-    public function setCommand(PoolCommand $command) : void{
-        $this->command = $command;
     }
 }
